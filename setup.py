6e89ee26c3ea75670b1fce42283532b76ec8d93d
Sun42 <sundas.christophe@gmail.com>
2016-10-06 20:48:19 +0100

    app structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='music_server',
+      version='0.1',
+      description='Download and listen music',
+      url='https://github.com/Sun42/music_server',
+      author='Christophe Sundas',
+      author_email='sundas.christophe@gmail.com',
+      license='MIT',
+      packages=['music_server',  'tests'],
+      zip_safe=False)
