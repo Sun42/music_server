6d285b16b75ff49d15c94f7ef46dedced37d5fce
Sun42 <sundas.christophe@gmail.com>
2016-10-06 21:00:08 +0100

    moving src files

--- converter.py
+++
@@ -1,9 +0,0 @@
-#!flask/bin/python
-
-import moviepy.editor as mp
-
-def video_to_mp3(video_filename):
-    clip = mp.VideoFileClip(video_filename)
-    audio_filename = video_filename + ".mp3"
-    clip.audio.write_audiofile(audio_filename)
-    return audio_filename
